0cb3bb0d76608394b2415a33db2b1b8e9dad1e67
Shivam <shivam@buffalo.edu>
2018-09-26 20:40:28 -0400

    updated Grid.h

--- SRC/Grid.h
+++ SRC/Grid.h
@@ -1,1 +1,38 @@
-// Code for generating grid
+#ifndef __GRID_H
+#define __GRID_H
+#define DIRICHLET 0
+#define NEUMANN 1
+#define INTERNAL 0
+#define BOUNDARY_LEFT 1
+#define BOUNDARY_RIGHT 2
+#define BOUNDARY_BOTTOM 3
+#define BOUNDARY_TOP 4
+#define GHOST_LEFT 5
+#define GHOST_RIGHT 6
+#define GHOST_BOTTOM 7
+#define GHOST_TOP 8
+
+struct Point{
+	int id = -1;
+	int x;
+	int y;
+	int type = INTERNAL;
+};
+
+class Grid{
+private:
+	double xrange[2];
+	double yrange[2];
+	void SetupGrid();
+public:
+	int nx, ny;
+	double hx, hy;
+	bool setup = false;
+	Point **pgrid;
+	Point **ugrid;
+	Point **vgrid;
+	Grid(char* fname);
+};
+
+
+#endif
