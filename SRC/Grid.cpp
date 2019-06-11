5fd1f55a79723226ba0c7bb5a9aab3b3963bbce7
Shivam Sinha <shivam@outis.fios-router.home>
2019-06-11 18:39:26 -0400

    Switching to a collocated grid

--- SRC/Grid.cpp
+++ SRC/Grid.cpp
@@ -1,91 +1,236 @@
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <cmath>
+#include <algorithm>
 #include "Grid.h"
 
- using namespace std;
+using namespace std;
 
- Grid::Grid(char* fname){
+Grid::Grid(char* fname){
 	ifstream fs{fname};
 	if(ParseDataFile(fs)) SetupGrid();
 	fs.close();
- }
+	ExportData();
+}
 
-/*
-Generates the grid points
-A staggered grid is used for u and v
-*/
- void Grid::SetupGrid(){
- 	if(xrange[0] >= xrange[1] || yrange[0] >= yrange[1]){
- 		cout << "Invalid domain range!\n";
- 		return;
+void Grid::SetupGrid(){
+ 	if(GenerateEdges() && GenerateFaces()) {
+ 		GenerateCells(); 
+ 		Cleanup();
+ 		Show();
+ 		setup = true;
  	}
+}
 
- 	if(nx <= 0 || ny <= 0){
- 		cout<<"Number of cells must be set to a positive number!\n";
- 		return;
- 	}
+//Generates edge information from vertices
+bool Grid::GenerateEdges(){
+ 	cout<<"Generating Edges...\n";
 
- 	hx = (xrange[1] - xrange[0])/nx;
- 	hy = (yrange[1] - yrange[0])/ny;
- 	
- 	pgrid = Points(ny+2, vector<Point>(nx+2));
- 	ugrid = Points(ny+2, vector<Point>(nx+3));
- 	vgrid = Points(ny+3, vector<Point>(nx+2));
+ 	vertices.push_back(vertices[0]);
 
- 	for(int i = 0; i < ny + 2; i++){
- 		for(int j = 0; j < nx + 2; j++){
- 			pgrid[i][j].x = xrange[0] -hx/2 + j*hx;
- 			pgrid[i][j].y = yrange[0] - hy/2 + i*hy;
- 			if (i%(ny+1) + j%(nx+1) == 0) 
- 				pgrid[i][j].type = GHOST_CORNER;
- 			else{
- 				if(i == 0) pgrid[i][j].type = GHOST_BOTTOM;
- 				else if(i == ny+1) pgrid[i][j].type = GHOST_TOP;
- 				else if(j == 0) pgrid[i][j].type = GHOST_LEFT;
- 				else if(j == nx+1) pgrid[i][j].type = GHOST_RIGHT;
+ 	for(int i = 1; i < vertices.size(); i++){
+ 		edges.push_back({});
+ 		if(vertices[i][0] == vertices[i-1][0]){
+ 			edges[i-1].loc[0] = vertices[i][0];
+ 			if(vertices[i][1] > vertices[i-1][1]){
+ 				edges[i-1].loc[1] = vertices[i-1][1];
+ 				edges[i-1].loc[2] = vertices[i][1];
+ 				edges[i-1].nx = -1;
+ 				xrange[0] = (vertices[i][0] < xrange[0])?vertices[i][0]:xrange[0];
  			}
+ 			else{
+ 				edges[i-1].loc[2] = vertices[i-1][1];
+ 				edges[i-1].loc[1] = vertices[i][1];
+ 				edges[i-1].nx = 1;
+ 				xrange[1] = (vertices[i][0] > xrange[1])?vertices[i][0]:xrange[1];
+ 			}
+ 		}
+ 		else if(vertices[i][1] == vertices[i-1][1]){
+ 			edges[i-1].loc[0] = vertices[i][1];
+ 			if(vertices[i][0] > vertices[i-1][0]){
+ 				edges[i-1].loc[1] = vertices[i-1][0];
+ 				edges[i-1].loc[2] = vertices[i][0];
+ 				edges[i-1].ny = 1;
+ 				yrange[1] = (vertices[i][1] > yrange[1])?vertices[i][1]:yrange[1];
+ 			}
+ 			else{
+ 				edges[i-1].loc[2] = vertices[i-1][0];
+ 				edges[i-1].loc[1] = vertices[i][0];
+ 				edges[i-1].ny = -1;
+ 				yrange[0] = (vertices[i][1] < yrange[0])?vertices[i][1]:yrange[0];
+ 			}
+ 		}
+ 		else{
+ 			cout<<"Edges should be parallel to the x-axis or y-axis\n";
+ 			return false;
  		}
  	}
 
- 	for(int i = 0; i < ny + 2; i++){
- 		for(int j = 0; j < nx + 3; j++){
- 			ugrid[i][j].x = xrange[0] + (j-1)*hx;
- 			ugrid[i][j].y = yrange[0] - hy/2 + i*hy;
- 			if (i%(ny+1) + j%(nx+2) == 0) 
- 				ugrid[i][j].type = GHOST_CORNER;
- 			else{
- 				if(i == 0) ugrid[i][j].type = GHOST_BOTTOM;
- 				else if(i == ny+1) ugrid[i][j].type = GHOST_TOP;
- 				else if(j == 0) ugrid[i][j].type = GHOST_LEFT;
- 				else if(j == nx+2) ugrid[i][j].type = GHOST_RIGHT;
- 				else if(j == 1) ugrid[i][j].type = BOUNDARY_LEFT;
- 				else if(j == nx+1) ugrid[i][j].type = BOUNDARY_RIGHT;
- 			}
+ 	return true;
+}
+
+//Generates cell faces
+bool Grid::GenerateFaces(){
+ 	cout<<"Generating Faces...\n";
+
+ 	X.push_back(xrange[0]);
+ 	Y.push_back(yrange[0]);
+
+ 	double h {0.0};
+ 	for(auto& i: Nx){
+ 		if(!equals(i[0], X.back()) || (X.size() == 1 && i[2] <= 0)) {
+ 			cout<<"Invalid specification for number of cells\n"; 
+ 			return false;
  		}
+ 		if(i[3] >0){
+ 			if(i[2] <= 0) i[2] = ceil(log((i[1]-i[0])*(i[3]-1)/h + 1)/log(i[3]));
+ 			h = (i[1]-i[0])*(i[3]-1)/(pow(i[3],i[2])-1);
+ 			double x = i[0]; 	
+ 			for(int j = 0; j<i[2]; j++) {X.push_back(x+=h); hx.push_back(h); h*=i[3];}		
+ 		}
+ 		else if(i[3] == -1){
+ 			if(i[2] <= 0) i[2] = ceil((i[1]-i[0])/h);
+ 			h = (i[1]-i[0])/i[2];
+ 			double x = i[0];
+ 			for(int j = 0; j<i[2]; j++) {X.push_back(x+=h); hx.push_back(h);}
+ 		}
+ 		else {cout<<"Invalid specification for number of cells\n"; return false;}
  	}
 
- 	for(int i = 0; i < ny + 3; i++){
- 		for(int j = 0; j < nx + 2; j++){
- 			vgrid[i][j].x = xrange[0] -hx/2 + j*hx;
- 			vgrid[i][j].y = yrange[0] + (i-1)*hy;
- 			if (i%(ny+2) + j%(nx+1) == 0) 
- 				vgrid[i][j].type = GHOST_CORNER;
- 			else{
- 				if(i == 0) vgrid[i][j].type = GHOST_BOTTOM;
- 				else if(i == ny+2) vgrid[i][j].type = GHOST_TOP;
- 				else if(j == 0) vgrid[i][j].type = GHOST_LEFT;
- 				else if(j == nx+1) vgrid[i][j].type = GHOST_RIGHT;
- 				else if(i == 1) vgrid[i][j].type = BOUNDARY_BOTTOM;
- 				else if(i == ny+1) vgrid[i][j].type = BOUNDARY_TOP;
+ 	for(auto& i: Ny){
+ 		if(!equals(i[0], Y.back()) || (Y.size() == 1 && i[2] <= 0)) {
+ 			cout<<"Invalid specification for number of cells\n"; 
+ 			return false;
+ 		}
+ 		if(i[3] >0){
+ 			if(i[2] <= 0) i[2] = ceil(log((i[1]-i[0])*(i[3]-1)/h + 1)/log(i[3]));
+ 			h = (i[1]-i[0])*(i[3]-1)/(pow(i[3],i[2])-1);
+ 			double x = i[0]; 	
+ 			for(int j = 0; j<i[2]; j++) {Y.push_back(x+=h); hy.push_back(h); h*=i[3];}		
+ 		}
+ 		else if(i[3] == -1){
+ 			if(i[2] <= 0) i[2] = ceil((i[1]-i[0])/h);
+ 			h = (i[1]-i[0])/i[2];
+ 			double x = i[0];
+ 			for(int j = 0; j<i[2]; j++) {Y.push_back(x+=h); hy.push_back(h);}
+ 		}
+ 		else {cout<<"Invalid specification for number of cells\n"; return false;}
+ 	}
+
+ 	if(!equals(xrange[1], X.back()) || !equals(yrange[1], Y.back())) {
+ 		cout<<"Invalid specification for number of cells\n"; 
+ 		return false;
+ 	}
+
+ 	return true;
+}
+
+//Constructs the discrete cells
+void Grid::GenerateCells(){
+ 	cout<<"Generating Cells...\n";
+
+ 	for(int i = 1; i < X.size(); i++){
+ 		cells.push_back(vector<Cell>(Y.size()-1));
+ 		for(int j = 1; j < Y.size(); j++){
+ 			Cell& cell = cells[i-1][j-1];
+ 			cell.X[0] = X[i-1];
+ 			cell.X[1] = X[i];
+ 			cell.Y[0] = Y[j-1];
+ 			cell.Y[1] = Y[j];
+ 			cell.x = 0.5*(cell.X[0]+cell.X[1]);
+ 			cell.y = 0.5*(cell.Y[0]+cell.Y[1]);
+ 		}
+ 	}
+}
+
+//Removes unnecessary cells
+void Grid::Cleanup(){
+ 	int n = 0;
+ 	for(auto& i: cells){
+ 		for(auto& j: i){
+ 			if(Interior(j)) {
+ 				j.id = n++;
+ 				double ar = (j.X[1]-j.X[0])/(j.Y[1]-j.Y[0]);
+ 				AR[0] = (AR[0] > ar)?ar:AR[0];
+ 				AR[1] = (AR[1] < ar)?ar:AR[1];
  			}
  		}
  	}
- 	setup = true;
- }
+ 	N = n;
+}
+
+//Checks whether a given cell lies inside the computational domain
+bool Grid::Interior(Cell& cell){
+ 	int intersect = 0;
+ 	for(int i = 0; i < edges.size(); i++){
+ 		Edge e = edges[i];
+ 		if(e.nx != 0){
+ 			if(cell.y > e.loc[1] && cell.y < e.loc[2]){
+ 				if(e.loc[0] > cell.x) intersect++;
+ 				if(e.nx == -1 && equals(cell.X[0], e.loc[0])) cell.edges.insert(i);
+ 				if(e.nx == 1 && equals(cell.X[1], e.loc[0])) cell.edges.insert(i);
+ 			} 
+ 		}
+ 		else{
+ 			if(cell.x > e.loc[1] && cell.x < e.loc[2]){
+ 				if(e.ny == -1 && equals(cell.Y[0], e.loc[0])) cell.edges.insert(i);
+ 				if(e.ny == 1 && equals(cell.Y[1], e.loc[0])) cell.edges.insert(i);
+ 			} 
+ 		}
+ 	}
+ 	if(intersect%2) return true;
+ 	return false;
+}
+
+//Outputs important information to the terminal
+void Grid::Show(){
+ 	cout<<setprecision(2);
+ 	ShowEdges();
+ 	cout<<endl;
+ 	cout<<"X:\t"<<xrange[0]<<"\t"<<xrange[1]<<endl;
+ 	cout<<"Y:\t"<<yrange[0]<<"\t"<<yrange[1]<<endl;
+ 	cout<<"AR:\t"<<AR[0]<<"\t"<<AR[1]<<endl;
+ 	cout<<"hx:\t"<<*min_element(hx.begin(),hx.end())<<"\t"
+ 					<<*max_element(hx.begin(),hx.end())<<endl;
+ 	cout<<"hy:\t"<<*min_element(hy.begin(),hy.end())<<"\t"
+ 					<<*max_element(hy.begin(),hy.end())<<endl;
+}
+
+//Prints information about the boundaries
+void Grid::ShowEdges(){
+ 	cout<<endl;
+ 	for(int i = 0; i < edges.size(); i++){
+ 		Edge e = edges[i];
+ 		cout<<"Edge\t"<<i<<":\t"<<"n\t"<<e.nx<<"\t"<<e.ny<<"\t";
+ 		if(e.nx != 0) cout<<"x\t"<<e.loc[0]<<"\ty\t"<<e.loc[1]<<"\t"<<e.loc[2]<<endl;
+ 		else cout<<"y\t"<<e.loc[0]<<"\tx\t"<<e.loc[1]<<"\t"<<e.loc[2]<<endl;
+ 	}
+}
+
+//Saves cell center information to a data file
+void Grid::ExportData(){
+ 	ofstream fs{"CellCenters.csv"};
+ 	for(auto& i: cells){
+ 		for(auto& j: i){
+ 			if(j.id != -1){
+ 				fs<<j.x<<","<<j.y;
+ 				for(int k = 0; k < edges.size(); k++){
+ 					if(j.edges.find(k) != j.edges.end()) fs<<","<<1;
+ 					else fs<<","<<0;
+ 				}
+ 				fs<<endl;
+ 			}
+ 		}
+ 	}
+ 	fs.close();
+}
 
 //Reads user-provided parameters from the grid data file
 bool Grid::ParseDataFile(ifstream& fs){
 	string inp{};
+	stringstream s;
 	bool err = false;
 
 	if(!fs){
@@ -96,8 +241,39 @@ bool Grid::ParseDataFile(ifstream& fs){
 	while(!fs.eof()){
 		fs>>inp;
 		if(fs.eof()) break;
-		if(inp == "domain") fs>>xrange[0]>>xrange[1]>>yrange[0]>>yrange[1];
-		else if(inp == "n") fs>>nx>>ny;
+		if(inp == "Vertices") {
+			if((fs>>ws).get() != '{') {err = true; break;}
+			while(fs.good()) {
+				if((fs>>ws).peek() == '}') {fs.ignore(); break;}
+				vertices.push_back({0,0});
+				fs>>vertices.back()[0]>>vertices.back()[1];
+				if(fs.eof()) {err = true; break;}
+			}
+		}
+		else if(inp == "Nx") {
+			if((fs>>ws).get() != '{') {err = true; break;}
+			while(fs.good()){
+				if((fs>>ws).peek() == '}') {fs.ignore(); break;}
+				Nx.push_back(vector<double>(4,-1));
+				getline(fs, inp);
+				s.str(inp);
+				s>>Nx.back()[0]>>Nx.back()[1]>>Nx.back()[2]>>Nx.back()[3];
+				s.clear();
+				if(fs.eof()) {err = true; break;}
+			}
+		}
+		else if(inp == "Ny") {
+			if((fs>>ws).get() != '{') {err = true; break;}
+			while(fs.good()){
+				if((fs>>ws).peek() == '}') {fs.ignore(); break;}
+				Ny.push_back(vector<double>(4,-1));
+				getline(fs, inp);
+				s.str(inp);
+				s>>Ny.back()[0]>>Ny.back()[1]>>Ny.back()[2]>>Ny.back()[3];
+				s.clear();
+				if(fs.eof()) {err = true; break;}
+			}
+		}
 		else {err = true; break;}
 
 		if(!fs.good()) {err = true; break;}
@@ -110,3 +286,6 @@ bool Grid::ParseDataFile(ifstream& fs){
 	return true;
 }
 
+bool Grid::equals(double a, double b){
+	return (abs(a-b) > TOL)?false:true;
+}
--- SRC/Grid.h
+++ SRC/Grid.h
@@ -3,39 +3,58 @@
 
 #include <fstream>
 #include <vector>
+#include <set>
 
 using namespace std; 
 
 enum bcTypes: int8_t{DIRICHLET, NEUMANN};
-enum pointTypes: int8_t{
-	INTERNAL, BOUNDARY_LEFT, BOUNDARY_RIGHT, BOUNDARY_BOTTOM, BOUNDARY_TOP,
-	GHOST_LEFT, GHOST_RIGHT, GHOST_BOTTOM, GHOST_TOP, GHOST_CORNER
+
+struct Edge{
+	int nx,ny; //Information on orientation of the boundaries
+	vector<double> loc {0,0,0}; //Information on the boundary location
+	int bcType;	//Type of boundary condition
+	vector<double> bcInfo;	//Information required for implementing the boundary condition
 };
 
-struct Point{
-	int id = -1;
-	double x;
-	double y;
-	int type = INTERNAL;
+typedef vector<Edge> EdgeList;
+
+struct Cell{
+	int id {-1};	//Number used to identify the cell
+	double x,y;	//Coordinates of the cell center
+	vector<double> X {0,0}; //Coordinates of the face centers
+	vector<double> Y {0,0};
+	set<int> edges;	//List of boundaries adjacent to the cell
 };
 
-typedef vector<vector<Point>> Points;
+typedef vector<vector<Cell>> CellList;
 
 class Grid{
 private:
-	double xrange[2];
-	double yrange[2];
+	const double TOL = 1E-8;
+	vector<double> xrange {1E15,-1E15};	//Extent of the computational domain
+	vector<double> yrange {1E15,-1E15};
+	vector<double> AR {1E15,-1E15};	//Minimum and maximum aspect ratios
+	vector<vector<double>> vertices; //Vertices of the computational domain
 	void SetupGrid();
+	bool GenerateEdges();
+	bool GenerateFaces();
+	void GenerateCells();
+	void Cleanup();
+	bool Interior(Cell&);
+	void Show();
+	void ShowEdges();
+	void ExportData();
 	bool ParseDataFile(ifstream& fs);
+	bool equals(double, double);
 public:
-	int nx = -1;	//number of cells in the x-direction for the u-grid
-	int ny = -1;	//number of cells in the y-direction for the v-grid
-	double hx, hy;	//grid spacing in x and y directions
+	int N; //Total number of cells
+	vector<vector<double>> Nx;	//Number of cells in the x-direction
+	vector<vector<double>> Ny;	//Number of cells in the y-direction
+	vector<double> X,Y;	//Location of the cell faces
+	vector<double> hx, hy;	//Grid spacing in x and y directions
+	CellList cells;	//List of cells in the grid
+	EdgeList edges;	//List of boundaries
 	bool setup = false;
-	Points pgrid;
-	Points ugrid;
-	Points vgrid;
-	int nPoints[3];
 	Grid(char* fname);
 };
 
